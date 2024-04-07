787a96c4309510ae150cb9d43927bf59fc52db2e
Camila Larissa <114198140+uaicamilalarissa@users.noreply.github.com>
2024-04-07 17:37:55 -0300

    Add files via upload
    Signed-off-by: Camila Larissa  <114198140+uaicamilalarissa@users.noreply.github.com>

---
+++ APIUAIFood/APIUAIFood/Models/Porções.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace APIUAIFood.Models
+{
+    public class Porções
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        [BsonElement("Nome")]
+        public string porçõesName { get; set; } = null!;
+
+        public decimal Preço { get; set; }
+
+        public string Tamanho { get; set; } = null!;
+
+        public string Categoria { get; set; } = null!;
+
+
+    }
+}
