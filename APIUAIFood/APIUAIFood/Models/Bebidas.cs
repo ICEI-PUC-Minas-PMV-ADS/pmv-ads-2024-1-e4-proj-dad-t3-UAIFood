7290ecd2bc3f6bb22bb624a84990e1818981ae4c
Hector Florença <90073811+Hectu13@users.noreply.github.com>
2024-04-07 17:33:22 -0300

    Add files via upload
    Signed-off-by: Hector Florença <90073811+Hectu13@users.noreply.github.com>

---
+++ APIUAIFood/APIUAIFood/Models/Bebidas.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace APIUAIFood.Models
+{
+    public class Bebidas
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        [BsonElement("Name")]
+        public string BebidasName { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        public string Tamanho { get; set; } = null!;
+
+        public string Categoria { get; set; } = null!;  
+
+    }
+}
