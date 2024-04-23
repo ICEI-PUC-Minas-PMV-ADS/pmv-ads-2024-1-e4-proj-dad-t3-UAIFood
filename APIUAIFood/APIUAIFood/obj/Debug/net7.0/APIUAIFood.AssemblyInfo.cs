a6b94740c24eade77f9fa127984e6221cb504a40
FernandoAAbreu <114960971+FernandoAAbreu@users.noreply.github.com>
2024-04-23 19:08:33 -0400

    commit

--- APIUAIFood/APIUAIFood/obj/Debug/net7.0/APIUAIFood.AssemblyInfo.cs
+++ APIUAIFood/APIUAIFood/obj/Debug/net7.0/APIUAIFood.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIUAIFood")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72173391e5057f0b3da0e7510d187323fee631de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIUAIFood")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIUAIFood")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
