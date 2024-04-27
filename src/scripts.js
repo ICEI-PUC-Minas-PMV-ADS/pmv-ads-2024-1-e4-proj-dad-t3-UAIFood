1be2ce5de46c120afd4ca2e1cba262344b7ca304
Hectu13 <hector.florenca@sga.pucminas.br>
2024-04-27 12:01:05 -0300

    Commit

--- src/scripts.js
+++ src/scripts.js
@@ -11,10 +11,24 @@ $(document).ready(function(){
   });
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+  // Adiciona um event listener para o botão Cancelar
+  document.getElementById("cancelar-btn").addEventListener("click", function() {
+    // Aqui você pode adicionar a lógica para cancelar a compra
+    alert("Compra cancelada!");
+    // Por exemplo, você pode fechar um modal, limpar o carrinho de compras, etc.
+  });
+});
+
 function comprar(nome, tamanho, valor) {
   var quantidade = parseInt(prompt("Quantidade desejada:"));
   if (!isNaN(quantidade) && quantidade > 0) {
     var total = quantidade * valor;
+    alert("Valor total \n"+
+    "Produto: " + nome + "\n" +
+    "Quantidade: " + quantidade + "\n" +
+    "Total: R$ " + total.toFixed(2));
+
     alert("Compra realizada!\n" + 
           "Produto: " + nome + "\n" +
           "Tamanho: " + tamanho + "\n" +
